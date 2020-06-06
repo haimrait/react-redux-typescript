aa18f1f9863550a0f72bfcdc869b887d6fce953a
Haim Raitsev <haimrait@gmail.com>
2020-06-06 15:18:40 +0300

    connect store to app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Photo, fetchPhotos } from '../actions';
+import { Store } from '../reducers';
 
-export class App extends React.Component {
+interface AppProps {
+  photos: Photo[];
+  fetchPhotos: Function;
+}
+
+class _App extends React.Component<AppProps> {
   render() {
     return <div>Hello World</div>;
   }
 }
+
+const mapStateToProps = ({ photos }: Store): { photos: Photo[] } => {
+  return { photos };
+};
+
+export const App = connect(mapStateToProps, { fetchPhotos })(_App);
